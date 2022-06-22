e3b4cbd89a45a02de07c02f76ee0fad71d03675b
Tan1sh <thecompany186@gmail.com>
2022-06-22 18:36:03 -0500

    Create prefixBase.js

---
+++ BASES/prefixBase.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: '', //name of the command
+    aliases: [], //aliases of the command
+    description: '', //description of the command
+    userPermissions: [], //user permissions required to use the command
+    botPermissions: [], //bot permissions required to use the command
+    disabled: false , //enable/disable the command
+    ownerOnly: false, //only the owner can use the command
+    args: false, //if the command needs arguments
+    usage: '', //use: 'Use in Spanish + Use in English'
+    
+    async execute(client, message, args, lang, languageFind, prefix) {
+        
+        //code here
+        
+    }
+}
