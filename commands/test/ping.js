058621a1dc69298e6920fa6a28b39eab87f11243
Tan1sh <thecompany186@gmail.com>
2022-06-22 18:55:58 -0500

    Create ping.js

---
+++ commands/test/ping.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'ping', //name of the command
+    aliases: [], //aliases of the command
+    description: '', //description of the command
+    userPermissions: [], //user permissions required to use the command
+    botPermissions: [], //bot permissions required to use the command
+    disabled: false , //enable/disable the command
+    ownerOnly: false, //only the owner can use the command
+    args: false, //if the command needs arguments
+    usage: '', //Use: 'Use in Spanish + Use in English'
+    
+    async execute(client, message, args, lang, languageFind, prefix) {
+        
+        message.channel.send({
+            content: 'pong'
+        })
+        
+    }
+}
